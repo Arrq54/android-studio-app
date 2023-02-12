2494877693f9e91e8b1c97949518a34896a7fa70
Arrq54 <arek.wojdyla@interia.pl>
2023-02-12 21:43:45 +0100

    cz 2 fin

--- client/app/src/main/java/com/example/arkadiuszapp001/ImageActivity.java
+++ client/app/src/main/java/com/example/arkadiuszapp001/ImageActivity.java
@@ -6,10 +6,6 @@ import android.content.Intent;
 import android.content.SharedPreferences;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
-import android.graphics.Canvas;
-import android.graphics.ColorMatrix;
-import android.graphics.ColorMatrixColorFilter;
-import android.graphics.Paint;
 import android.graphics.drawable.BitmapDrawable;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
@@ -160,54 +156,54 @@ public class ImageActivity extends AppCompatActivity {
         });
 
 
-        seekbarDiv = findViewById(R.id.seekbarDiv);
-        seekbar = findViewById(R.id.seekbar);
+//        seekbarDiv = findViewById(R.id.seekbarDiv);
+//        seekbar = findViewById(R.id.seekbar);
 
 
 
-        ImageView brightness = findViewById(R.id.brightnessbn);
-        brightness.setOnClickListener(v->{
-            if(  seekbarType == "brightness"){
-                seekbarDiv.setAlpha(0f);
-            }else{
-                seekbarDiv.setAlpha(1f);
-                seekbarType = "brightness";
-                seekbar.setProgress((int) brightnessProgress);
-            }
+//        ImageView brightness = findViewById(R.id.brightnessbn);
+//        brightness.setOnClickListener(v->{
+//            if(  seekbarType == "brightness"){
+//                seekbarDiv.setAlpha(0f);
+//            }else{
+//                seekbarDiv.setAlpha(1f);
+//                seekbarType = "brightness";
+//                seekbar.setProgress((int) brightnessProgress);
+//            }
+//
+//
+//
+//
+//        });
+//
+//        ImageView contrast = findViewById(R.id.contrast);
+//        contrast.setOnClickListener(v->{
+//            if(  seekbarType == "contrast"){
+//                seekbarDiv.setAlpha(0f);
+//            }else{
+//                seekbarDiv.setAlpha(1f);
+//                seekbarType = "contrast";
+//                seekbar.setProgress((int) contrastProgress);
+//            }
 
 
 
-
-        });
-
-        ImageView contrast = findViewById(R.id.contrast);
-        contrast.setOnClickListener(v->{
-            if(  seekbarType == "contrast"){
-                seekbarDiv.setAlpha(0f);
-            }else{
-                seekbarDiv.setAlpha(1f);
-                seekbarType = "contrast";
-                seekbar.setProgress((int) contrastProgress);
-            }
-
-
-
-
-        });
-
-        ImageView saturation = findViewById(R.id.saturation);
-        saturation.setOnClickListener(v->{
-            if(  seekbarType == "saturation"){
-                seekbarDiv.setAlpha(0f);
-            }else{
-                seekbarDiv.setAlpha(1f);
-                seekbarType = "saturation";
-                seekbar.setProgress((int) saturationProgress);
-            }
-
-
-
-        });
+//
+//        });
+//
+//        ImageView saturation = findViewById(R.id.saturation);
+//        saturation.setOnClickListener(v->{
+//            if(  seekbarType == "saturation"){
+//                seekbarDiv.setAlpha(0f);
+//            }else{
+//                seekbarDiv.setAlpha(1f);
+//                seekbarType = "saturation";
+//                seekbar.setProgress((int) saturationProgress);
+//            }
+//
+//
+//
+//        });
 
 
         //RYSOWANIE
@@ -236,72 +232,72 @@ public class ImageActivity extends AppCompatActivity {
 
 
 
-        final Bitmap[] bitmap = {((BitmapDrawable) a.getDrawable()).getBitmap()};
-
-        Bitmap toChange = bitmap[0].copy(bitmap[0].getConfig(),true);
-        seekbar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
-            @Override
-            public void onProgressChanged(SeekBar seekBar, int i, boolean b) {
-                if(seekbarGoing){
-                    float tab[];
-                    Bitmap editBmp = bitmap[0].copy(bitmap[0].getConfig(),true);
-                    ColorMatrix cMatrix = new ColorMatrix();
-
-                    if(seekbarType=="brightness"){
-                        brightnessValue = (seekBar.getProgress()/200f)*100;
-
-                    }else if (seekbarType == "contrast"){
-                        contrastValue = (seekBar.getProgress()/100f)*1;
-                    }
-                    else if (seekbarType=="saturation"){
-                        saturationValue = (seekBar.getProgress());
-                    }
-
-
-                    tab = new float[] {
-                            contrastValue, 0, 0, 0, brightnessValue,
-                            0, contrastValue, 0, 0, brightnessValue,
-                            0, 0, contrastValue, 0, brightnessValue,
-                            0, 0, 0, 1, 0 };
-
-
-                    cMatrix.setSaturation(Float.parseFloat(String.valueOf(saturationValue)));
-
-                    cMatrix.set(tab);
-                    Paint paint = new Paint();
-                    paint.setColorFilter(new ColorMatrixColorFilter(cMatrix));
-                    Canvas canvas = new Canvas(editBmp);
-                    canvas.drawBitmap(editBmp, 0, 0, paint);
-                    a.setImageBitmap(editBmp);
-                }
-
-            }
-
-            @Override
-            public void onStartTrackingTouch(SeekBar seekBar) {
-                seekbarGoing=true;
-            }
-
-            @Override
-            public void onStopTrackingTouch(SeekBar seekBar) {
-                seekbarGoing = false;
-//                bitmap[0] = ((BitmapDrawable) a.getDrawable()).getBitmap();
-                switch(seekbarType){
-                    case "brightness":
-                        brightnessProgress = seekBar.getProgress();
-                        break;
-
-                    case "contrast":
-                        contrastProgress = seekBar.getProgress();
-                        break;
-                    case "saturation":
-                        saturationProgress = seekbar.getProgress();
-                        break;
-                    default: break;
-
-                }
-            }
-        });
+//        final Bitmap[] bitmap = {((BitmapDrawable) a.getDrawable()).getBitmap()};
+//
+//        Bitmap toChange = bitmap[0].copy(bitmap[0].getConfig(),true);
+//        seekbar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+//            @Override
+//            public void onProgressChanged(SeekBar seekBar, int i, boolean b) {
+//                if(seekbarGoing){
+//                    float tab[];
+//                    Bitmap editBmp = bitmap[0].copy(bitmap[0].getConfig(),true);
+//                    ColorMatrix cMatrix = new ColorMatrix();
+//
+//                    if(seekbarType=="brightness"){
+//                        brightnessValue = (seekBar.getProgress()/200f)*100;
+//
+//                    }else if (seekbarType == "contrast"){
+//                        contrastValue = (seekBar.getProgress()/100f)*1;
+//                    }
+//                    else if (seekbarType=="saturation"){
+//                        saturationValue = (seekBar.getProgress());
+//                    }
+//
+//
+//                    tab = new float[] {
+//                            contrastValue, 0, 0, 0, brightnessValue,
+//                            0, contrastValue, 0, 0, brightnessValue,
+//                            0, 0, contrastValue, 0, brightnessValue,
+//                            0, 0, 0, 1, 0 };
+//
+//
+//                    cMatrix.setSaturation(Float.parseFloat(String.valueOf(saturationValue)));
+//
+//                    cMatrix.set(tab);
+//                    Paint paint = new Paint();
+//                    paint.setColorFilter(new ColorMatrixColorFilter(cMatrix));
+//                    Canvas canvas = new Canvas(editBmp);
+//                    canvas.drawBitmap(editBmp, 0, 0, paint);
+//                    a.setImageBitmap(editBmp);
+//                }
+//
+//            }
+//
+//            @Override
+//            public void onStartTrackingTouch(SeekBar seekBar) {
+//                seekbarGoing=true;
+//            }
+//
+//            @Override
+//            public void onStopTrackingTouch(SeekBar seekBar) {
+//                seekbarGoing = false;
+////                bitmap[0] = ((BitmapDrawable) a.getDrawable()).getBitmap();
+//                switch(seekbarType){
+//                    case "brightness":
+//                        brightnessProgress = seekBar.getProgress();
+//                        break;
+//
+//                    case "contrast":
+//                        contrastProgress = seekBar.getProgress();
+//                        break;
+//                    case "saturation":
+//                        saturationProgress = seekbar.getProgress();
+//                        break;
+//                    default: break;
+//
+//                }
+//            }
+//        });
 
 
     }
